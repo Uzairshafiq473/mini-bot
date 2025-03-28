7b2a10e99e7a400ad1fdb0bf243dbca0cf0afadd
Muhammad Uzair Shafiq <uzairshafiq473@gmail.com>
2025-03-29 04:14:01 +0500

    push

--- api-server/server.js
+++ api-server/server.js
@@ -35,5 +35,5 @@ app.post('/api/openrouter', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
